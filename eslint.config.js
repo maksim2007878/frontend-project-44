dc25edd7f76acf0b351176062151df5ddf57021d
LizaDesh <liza.deh2007@gmail.com>
2026-04-02 12:34:37 +0700

    2

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,7 @@
-import js from '@eslint/js';
-import globals from 'globals'; 
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
 
-export default [
-  js.configs.recommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node, 
-      },
-    },
-    rules: {
-    },
-  },
-];
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+]);
